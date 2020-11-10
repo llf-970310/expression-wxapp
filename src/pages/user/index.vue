aed90a9655720e546b4a06fca9b8830b24176b32
Longfei Li <lf97310@gmail.com>
2020-11-10 23:11:13 +0800

    feat: use invitation code

--- src/pages/user/index.vue
+++ src/pages/user/index.vue
@@ -25,11 +25,24 @@
                         class="list_content"
                     >{{userInfo.lastLoginTime == null ? "--" : userInfo.lastLoginTime}}</view>
                 </view>
+            </view>
+
+            <view class="list_view">
+                <view class="list">
+                    <view class="list_title">VIP 过期时间</view>
+                    <view
+                        class="list_content"
+                    >{{userInfo.vipEndTime == null ? "--" : userInfo.vipEndTime}}</view>
+                </view>
                 <view class="list">
                     <view class="list_title">剩余测试次数</view>
                     <view
                         class="list_content"
                     >{{userInfo.examNum == null ? "--" : userInfo.examNum}}</view>
+                </view>
+                <view class="list" @tap="useInvitationCode">
+                    <view class="list_title">使用邀请码</view>
+                    <AtIcon value="chevron-right" size="20" color="#7f7f7f" />
                 </view>
             </view>
 
@@ -39,6 +52,35 @@
                     <AtIcon value="chevron-right" size="20" color="#7f7f7f" />
                 </view>
             </view>
+
+            <!-- <AtFloatLayout :isOpened="floatLayoutOpen" :onClose="onFloatClose">
+                <AtForm>
+                    <AtInput
+                        type="text"
+                        placeholder="请输入邀请码"
+                        :value="invitationCode"
+                        :onChange="handleInput"
+                    >
+                        <view style="width: 90px" :onClick="updatePrivilege">确定</view>
+                    </AtInput>
+                </AtForm>
+            </AtFloatLayout>-->
+
+            <AtModal :isOpened="isModelOpened">
+                <AtModalContent>
+                    <text>请输入邀请码：</text>
+                    <AtInput
+                        type="text"
+                        :value="invitationCode"
+                        :onChange="handleInput"
+                        style="margin-left: 0"
+                    ></AtInput>
+                </AtModalContent>
+                <AtModalAction>
+                    <button @tap="closeModal">取消</button>
+                    <button @tap="updatePrivilege">确定</button>
+                </AtModalAction>
+            </AtModal>
         </view>
     </view>
 </template>
@@ -46,6 +88,7 @@
 <script>
 import "./index.scss";
 import api from "../../util/api.js";
+import Taro from "@tarojs/taro";
 
 export default {
     data() {
@@ -53,7 +96,10 @@ export default {
             toastText: "",
             toastShow: false,
             userInfo: {},
-            dataLoading: true
+            dataLoading: true,
+
+            isModelOpened: false,
+            invitationCode: ""
         };
     },
     mounted() {
@@ -83,6 +129,46 @@ export default {
                     this.userInfo.registerTime = res.data.data.register_time;
                     this.userInfo.lastLoginTime = res.data.data.last_login_time;
                     this.userInfo.examNum = res.data.data.remaining_exam_num;
+                    this.userInfo.vipEndTime = res.data.data.vip_end_time;
+                }
+            });
+        },
+
+        closeModal() {
+            this.isModelOpened = false;
+            this.invitationCode = "";
+        },
+
+        useInvitationCode() {
+            this.isModelOpened = true;
+        },
+
+        handleInput(value) {
+            this.invitationCode = value;
+        },
+
+        updatePrivilege() {
+            api.post(
+                "/api/account/update-privilege/" + this.invitationCode
+            ).then(res => {
+                if (res.data.code != 0) {
+                    Taro.showToast({
+                        title: res.data.msg,
+                        icon: "none",
+                        duration: 1300
+                    });
+                } else {
+                    this.closeModal();
+                    Taro.showToast({
+                        title: "使用成功",
+                        icon: "none",
+                        duration: 1300
+                    });
+                    setTimeout(() => {
+                        Taro.redirectTo({
+                            url: `/pages/user/index`
+                        });
+                    }, 1300);
                 }
             });
         }
